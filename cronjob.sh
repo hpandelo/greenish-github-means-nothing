393972ca92663234fbaf1b68ea83b0c138f522c1
Helcio Pandelo <hmacedo2007@gmail.com>
2023-09-15 03:00:00 -0300

    Greenish Github MEANS NOTHING and I can proof it

--- cronjob.sh
+++ cronjob.sh
@@ -5,6 +5,10 @@ cd ~/workspace/greenish-github-means-nothing
 # Remove last file to prevent getting a giant and unecessary repo
 rm ProofOfThat.txt
 
+# Why not to have a loop here??
+# Randomly from 1 to 10 commits to make our greenish github more "colorful" (50 shades of Green? haha)
+# Contribute with your PR since I'm already tired today and too lazy to continue working now =P
+
 # Write a new file with current date
 echo $(date) '- Always Remember: Fking Greenish Github MEANS NOTHING' >> ProofOfThat.txt
 
