79c44da399497d681e100152f1aff4e0dfb8650f
Helcio Pandelo <hmacedo2007@gmail.com>
2025-01-27 17:25:52 -0300

    feat: random commits

--- cronjob.sh
+++ cronjob.sh
@@ -1,18 +1,22 @@
-#! /bin/bash
-# Enter in project folder
-cd ~/workspace/greenish-github-means-nothing
+#!/bin/bash
 
-# Remove last file to prevent getting a giant and unecessary repo
-rm ProofOfThat.txt
+# Enter the project folder
+cd ~/workspace/greenish-github-means-nothing || { echo "Failed to change directory"; exit 1; }
 
-# Why not to have a loop here??
-# Randomly from 1 to 10 commits to make our greenish github more "colorful" (50 shades of Green? haha)
-# Contribute with your PR since I'm already tired today and too lazy to continue working now =P
+# Remove the last file to prevent getting a giant and unnecessary repo
+rm -f ProofOfThat.txt
 
-# Write a new file with current date
-echo $(date) '- Always Remember: Fking Greenish Github MEANS NOTHING' >> ProofOfThat.txt
+# Generate a random number of commits between 1 and 10
+commit_count=$((RANDOM % 10 + 1))
 
-# Perform the git command to have a new commit
-/usr/bin/git add .
-/usr/bin/git commit -m 'Greenish Github MEANS NOTHING and I can proof it'
+for ((i = 1; i <= commit_count; i++)); do
+  # Write a new file with the current date
+  echo "$(date) - Always Remember: Fking Greenish Github MEANS NOTHING" >> ProofOfThat.txt
+
+  # Perform the git commands to create a new commit
+  /usr/bin/git add ProofOfThat.txt
+  /usr/bin/git commit -m "Greenish Github MEANS NOTHING and I can prove it - Commit #$i"
+done
+
+# Push the commits
 /usr/bin/git push
